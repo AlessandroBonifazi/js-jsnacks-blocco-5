ce8802442f6097fd466b5d25929b30024abecee0
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-22 17:14:17 +0100

    jsnack 3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,6 +1,22 @@
 // Debug
 console.log('JS OK!');
 
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata
+// (es. Ciao -> oaiC)
+
 // Config
+const result = document.getElementById('result');
 
 // Functions
+function reverseString(string) {
+    let wordReverse = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        wordReverse += string[i]
+    }
+
+    return wordReverse;
+}
+
+// reverseString('hello');
+result.innerText = reverseString('hello');
