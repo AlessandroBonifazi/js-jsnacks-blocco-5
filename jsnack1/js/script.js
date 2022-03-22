8ff0c12b55c9c0dc3682d125bd5797915cfff530
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-22 17:01:03 +0100

    jsnack 1

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,6 +1,75 @@
 // Debug
 console.log('JS OK!');
 
+// Crea un array di 10 oggetti che rappresentano una zucchina,
+// indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
 // Config
+const zucchine = [
+    {
+        variety: 'Zucchina nera',
+        weight: 150,
+        length: 20,
+    },
+    {
+        variety: 'Zucchino romanesco',
+        weight: 100,
+        length: 20,
+    },
+    {
+        variety: 'Zucchino fiorentino',
+        weight: 80,
+        length: 20,
+    },
+    {
+        variety: 'Zucchina napoletana',
+        weight: 70,
+        length: 20,
+    },
+    {
+        variety: 'Zucchine tonde',
+        weight: 110,
+        length: 20,
+    },
+    {
+        variety: 'Zucchine trombetta',
+        weight: 60,
+        length: 20,
+    },
+    {
+        variety: 'Zucchina pâtisson',
+        weight: 130,
+        length: 20,
+    },
+    {
+        variety: 'Zucchino giallo',
+        weight: 90,
+        length: 20,
+    },
+    {
+        variety: 'Zucchino rugoso friulano',
+        weight: 75,
+        length: 20,
+    },
+    {
+        variety: 'Zucchina Crookneck',
+        weight: 160,
+        length: 20,
+    },
+];
+
+const result = document.getElementById('result');
 
 // Functions
+function calcTotWeight() {
+    let totWeight = 0;
+
+    for (let i = 0; i < zucchine.length; i++) {
+        totWeight += zucchine[i].weight;
+        result.innerText = 'Total Weight: ' + totWeight;
+    }
+    return totWeight;
+}
+
+calcTotWeight()
